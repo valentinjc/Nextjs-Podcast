8b89145438c9f20a9a8c2e4e34399cf436fbb255
valentinjc <56897335+valentinjc@users.noreply.github.com>
2020-01-30 14:46:41 -0600

    First

---
+++ pages/index.js
@@ -0,0 +1,73 @@
+import 'isomorphic-fetch';
+import Link from 'next/link';
+
+export default class extends React.Component {
+    
+    static async getInitialProps(){
+        let req = await fetch
+        ('https://api.audioboom.com/channels/recommended')
+
+        //Busca la variable Body y trae la ficha channels
+        let {body:channels} = await req.json();
+        
+        return {channels};
+    }
+    render () {
+        const { channels } = this.props;
+        return <div>
+            <header>
+                Podcast
+            </header>
+            <div className = "channels">
+                { channels.map((channel)=>(
+                    <Link href={`/channel?id=${channel.id}`}>
+                        <a className ="channel">
+                            <img src={ channel.urlss.logo_image.original} alt=""/>
+                            <h2> { channel.title } </h2>
+                        </a>
+                    </Link>
+                ))}
+            </div>
+            <style jsx>{`
+                header {
+                color: #fff;
+                background: #8756ca;
+                padding: 15px;
+                text-align: center;
+                }
+                .channels {
+                display: grid;
+                grid-gap: 15px;
+                padding: 15px;
+                grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+                }
+                a.channel {
+                display: block;
+                margin-bottom: 0.5em;
+                color: #333;
+                text-decoration: none;
+                }
+                .channel img {
+                border-radius: 3px;
+                box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+                width: 100%;
+                }
+                h2 {
+                padding: 5px;
+                font-size: 0.9em;
+                font-weight: 600;
+                margin: 0;
+                text-align: center;
+                }
+            `}</style>
+
+            <style jsx global>{`
+                body {
+                margin: 0;
+                font-family: system-ui;
+                background: white;
+                }
+            `}</style>
+        </div>
+    }
+}
---
+++ pages/podcast.js
@@ -0,0 +1,7 @@
+export default class extends React.Component{
+    static async getInitialProps({query}){
+        let id = query.id
+        return {};
+    }
+    
+}
